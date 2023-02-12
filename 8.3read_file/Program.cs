fd604f3374e27f2f309f6708f551284029105005
Yulia Miller <miller82.98@mail.ru>
2023-02-11 21:18:38 -0800

    Добавьте файлы проекта.

---
+++ 8.3read_file/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class FileWriter
+{
+    public static void Main()
+    {
+        string filePath = @"/Users/mille/SkillFactory/student.txt";
+        if(!File.Exists(filePath))
+        {
+            using(StreamWriter sw = File.CreateText(filePath))
+            {
+                sw.WriteLine("Юлия");
+                sw.WriteLine("Андрей");
+                sw.WriteLine("Артем");
+            }
+        }
+        using (StreamReader sr = File.OpenText(filePath))
+        {
+            string str = "";
+            while((str = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
